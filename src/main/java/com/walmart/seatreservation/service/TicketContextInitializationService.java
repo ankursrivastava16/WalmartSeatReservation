8e68a94e04ee7bdba597bb4c170b76102b90e6a1
ankursrivastava16 <33137020+ankursrivastava16@users.noreply.github.com>
2017-11-28 19:46:07 -0500

    Delete TicketContextInitializationService.java

--- src/main/java/com/walmart/seatreservation/service/TicketContextInitializationService.java
+++
@@ -1,9 +0,0 @@
-package com.walmart.seatreservation.service;
-
-import com.walmart.seatreservation.model.Arena;
-
-public interface TicketContextInitializationService { 
-	
-	Arena initArena();
-
-}
